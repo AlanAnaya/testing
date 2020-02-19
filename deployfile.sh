0a72d0d3a2cd8511712a94aaf7b2121fea2f3d8a
AlanAnaya <52431937+AlanAnaya@users.noreply.github.com>
2020-02-18 18:28:59 -0700

    Update deployfile.sh

--- deployfile.sh
+++ deployfile.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-chmod 777 -r /etc/sysconfig/jenkins 
+chown jenkins -r /etc/sysconfig/jenkins 
 sed -i -e 's/JENKINS_USER\="jenkins"/JENKINS_USER\="root"/g' /etc/sysconfig/jenkins
 scp -r target/surefire-reports/*.xml cloud_user@alananaya3c.mylabserver.com:/home/cloud_user/Desktop
