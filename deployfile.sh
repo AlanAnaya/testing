733705497ebd37f0f7d1318cd0d1efdbae5ade9c
AlanAnaya <52431937+AlanAnaya@users.noreply.github.com>
2020-02-18 18:43:00 -0700

    Update deployfile.sh

--- deployfile.sh
+++ deployfile.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-scp -r target/surefire-reports/*.xml cloud_user@alananaya3c.mylabserver.com:/home/cloud_user/Desktop
+scp -r cloud_user@alananaya4c.mylabserver.com:target/surefire-reports/*.xml cloud_user@alananaya3c.mylabserver.com:/home/cloud_user/Desktop
